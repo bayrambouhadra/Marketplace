1de2c676d4e297f2be4ed82cf2060de8c437e2b7
bayrambouhadra <gaymingthe@gmail.com>
2024-05-24 21:08:13 +0200

    first commit

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ const products = [
     { name: "White Shark HEADSET GH-1841 LION", price: 110, image: "casque-micro-gaming-white-shark-lion-gh-1841-silver.jpg" },
     { name: "white shark Microphone Gamer Nagara DSM-02", price: 299, image: "1.jpg" },
     { name: "RTX 4090", price: 8000, image: "asus-rog-strix-rtx-4090-24gb.jpg" },
-    { name: "playstation 5 controller", price: 359, image: "r.jpg" },
+    { name: "playstation 5 controller", price: 359, image: "61uQKdWCfAL._AC_UF1000,1000_QL80_.jpg" },
+    { name: "Gigabyte GeForce RTX 3060 Ti", price: 1899 , image: "téléchargement.jfif" },
+    { name: "Elgato Stream Deck Mini", price: 59 , image: "615JEWGsK1L._AC_UF894,1000_QL80_.jpg" },  
+    { name: "Razer Cobra Pro Wireless Gaming Mouse", price: 119.73 , image: "71tuAoPh-LL._AC_UF894,1000_QL80_.jpg" },
+    { name: "Gaming PC", price: 1465.45 , image: "png-clipart-intel-megaport-gaming-computer-personal-computer-intel-electronics-computer-thumbnail.png", description: "- GeForce RTX 4080 | AMD Ryzen 7 5800X3D " },
+    { name: "Razer RZ03-04470200-R3U1 Ornata V3 X Gaming Keyboard", price: 38 , image: "71pqfvJKW5L._AC_SL1500_.jpg" },
+    { name: "Gaming chair", price: 145 , image: "71+hOg9faYL._AC_SL1500_.jpg" },
 ];
 
 const productContainer = document.querySelector('.product-list');
@@ -20,10 +26,12 @@ products.forEach((product, index) => {
         <img src="${product.image}" alt="${product.name}">
         <h2>${product.name}</h2>
         <p>Price: $${product.price}</p>
+        ${product.name === "Gaming PC" ? `<button class="show-more">View More</button>` : ''}
         <button class="add-to-cart" data-index="${index}">Add to Cart</button>
     `;
     productContainer.appendChild(productCard);
 });
+
 document.querySelectorAll('.add-to-cart').forEach(button => {
     button.addEventListener('click', () => {
         const index = parseInt(button.dataset.index);
@@ -34,7 +42,11 @@ document.querySelectorAll('.add-to-cart').forEach(button => {
     });
 });
 
-
+document.querySelector('.show-more').addEventListener('click', () => {
+    const productIndex = products.findIndex(product => product.name === "Gaming PC");
+    const product = products[productIndex];
+    alert(product.description);
+});
 
 function updateCart() {
     cartItemsList.innerHTML = '';
